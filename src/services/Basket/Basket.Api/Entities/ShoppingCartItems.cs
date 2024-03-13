dd16b639483c1ea64a308c9b7396d9d5157b1fe4
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-03-13 23:15:53 +0200

    Create Entities For Basket.Api MicroService

---
+++ src/services/Basket/Basket.Api/Entities/ShoppingCartItems.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCartItems
+    {
+        public string ProductId { get; set; }
+        public string  ProductName{ get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+
+    }
+}
